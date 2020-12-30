2ea69349b474166486973f209f2067a2dfe77d11
Mathias Nonohay <mathias12300@gmail.com>
2020-12-30 15:50:43 -0300

    tela Pacotes melhorada e celulas receberam informações

---
+++ alura-viagens/Pacotes/View/PacoteViagemCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PacoteViagemCollectionViewCell.swift
+//  alura-viagens
+//
+//  Created by Mathias Almeida Nonohay on 12/30/20.
+//  Copyright © 2020 Mathias Almeida Nonohay. All rights reserved.
+//
+
+import UIKit
+
+class PacoteViagemCollectionViewCell: UICollectionViewCell {
+
+    // MARK: - IBOutlet
+    
+    @IBOutlet weak var imagemViagem: UIImageView!
+    @IBOutlet weak var labelTitulo: UILabel!
+    @IBOutlet weak var labelQuantidadeDeDias: UILabel!
+    @IBOutlet weak var labelPreco: UILabel!
+    
+    
+    
+    
+    
+}
