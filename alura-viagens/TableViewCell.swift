765ed7fc00c968f97b12d54ae18d95030c10e765
Mathias Nonohay <mathias12300@gmail.com>
2020-12-30 09:22:17 -0300

    primeiro commit

---
+++ alura-viagens/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  alura-viagens
+//
+//  Created by Mathias Almeida Nonohay on 12/30/20.
+//  Copyright © 2020 Mathias Almeida Nonohay. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ alura-viagens/Viagem.swift
@@ -0,0 +1,13 @@
+//
+//  Viagem.swift
+//  alura-viagens
+//
+//  Created by Mathias Almeida Nonohay on 12/29/20.
+//  Copyright © 2020 Mathias Almeida Nonohay. All rights reserved.
+//
+
+import UIKit
+
+class Viagem: NSObject {
+
+}
